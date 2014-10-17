de38cee203a099d0f7753f252cbb26d3e2dc6baa
Døkkarr Hirðisson <dokkarr@lfe.io>
2014-10-17 13:20:06 -0500

    Removed copied compile target adendum.

--- resources/make/common.mk
+++ resources/make/common.mk
@@ -99,7 +99,6 @@ compile: get-deps clean-ebin copy-appsrc
 	@which rebar.cmd >/dev/null 2>&1 && \
 	PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) rebar.cmd compile || \
 	PATH=$(SCRIPT_PATH) ERL_LIBS=$(ERL_LIBS) rebar compile
-	@cd deps/lutil && make compile
 
 compile-no-deps: clean-ebin
 	@echo "Compiling only project code ..."
