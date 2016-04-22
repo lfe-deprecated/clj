008611c5b4f0861996362359daf52ca660b7fbc8
Duncan McGreggor <oubiwann@gmail.com>
2016-04-22 11:44:46 -0500

    Another try.

--- priv/scripts/setup_test_env.sh
+++ priv/scripts/setup_test_env.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
+REBAR=`PATH=.:$PATH which rebar3`
 LFE_TEST_BUILD=`pwd`/_build/test
 cd ${LFE_TEST_BUILD}/lib/lutil && \
 mkdir -p _build && \
 (test -e _build/default || \
 ln -s ${LFE_TEST_BUILD} _build/default)
 cd ${LFE_TEST_BUILD}/lib/lutil && \
-PATH=.:$PATH rebar3 compile || echo "Already compiled."
+$REBAR compile || echo "Already compiled."
